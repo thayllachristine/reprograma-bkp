5bc47454b36ad866c93d515267f5ee8b4b8c926d
Tiago Fernando Sarri <tiagosarri@hotmail.com>
2018-09-21 18:22:11 -0300

    * alteracao do script e adicionando os comentarios sobre as funcionalidades da manipulacao

--- Semana 7/3 - Manipulação de estilos/2 - Menu/script/script.js
+++ Semana 7/3 - Manipulação de estilos/2 - Menu/script/script.js
@@ -1,50 +1,52 @@
-const menuGeneral = document.querySelector(`.sidebar-menu`)
+const menuGeneral = document.querySelector(`.sidebar-menu`);
 
-const menuButton = document.querySelector(`.sidebar-menu__button`)
+const menuButton = document.querySelector(`.sidebar-menu__button`);
 
 menuButton.addEventListener(`click`, function(event){
     event.preventDefault()
 
 
-    // const verifyPosition = (menuGeneral.style.left = `-34%`)
+    //aqui a sua variavel será do tipo objeto, porque tem os colchetes
+    var verifyPosition = [menuGeneral.style.left = `-34%`]
+    console.log(verifyPosition[0])
+    console.log(typeof verifyPosition) //vai retornar o tipo objeto ou seja [colchete] ou array de string
 
-    // console.log(menuGeneral)
-    // console.log(verifyPosition)
-
-
-    // if (verifyPosition < 0){
-    //     console.log(verifyPosition)
-    //     console.log(`AAAAAAAAAAa`)
-    // } else {
-    //     // verifyPosition
-    //     console.log(`BBBBBBB`)
-    //     console.log(verifyPosition, `dentro do if`)
-    // }
-
-    // menuGeneral.style.left = `-34%`
-    // menuGeneral.style.boxShadow = `none`
-
-    // if (menuGeneral.style.left === `-34%`){ 
-    //     menuGeneral.style.left = `0%`
-    // } else {
-    //     menuGeneral.style.left = `-34%`
-    // }
-
-
-    const verifyPosition = (menuGeneral.style.left = `-34%`)
-
-    console.log(verifyPosition)
-
-    if (verifyPosition < `0%`) {
-        // verifyPosition = `0%`
+    if (verifyPosition[0] === `-34%`) {
         menuGeneral.style.left = `0%`
-        console.log(menuGeneral)
-        console.log(`AAAAaaa`)
+        console.log("1"); //vai printar este console
     } else {
-        menuGeneral.style.left = `-34%` 
-        console.log(`BBBBBBBB`)
+        menuGeneral.style.left = `-44%` 
+        console.log("2");
     }
 
-})
+    //pego a mesma variavel, e faço de outro forma, sem os colchetes neste a variavel sera string
+    verifyPosition = (menuGeneral.style.left = `-34%`)
+    console.log(verifyPosition)
+    console.log(typeof verifyPosition) //vai retornar o tipo string porque esta sem os [colchete]
 
+    if (verifyPosition === `-34%`) {
+        menuGeneral.style.left = `0%`
+        console.log("1"); //vai printar este console
+    } else {
+        menuGeneral.style.left = `-44%` 
+        console.log("2");
+    }    
+
+
+
+    // const verifyPosition = (menuGeneral.style.left = `-34%`)
+
+    // console.log(verifyPosition)
+
+    // if (verifyPosition < `0%`) {
+    //     // verifyPosition = `0%`
+    //     menuGeneral.style.left = `0%`
+    //     console.log(menuGeneral)
+    //     console.log(`AAAAaaa`)
+    // } else {
+    //     menuGeneral.style.left = `-34%` 
+    //     console.log(`BBBBBBBB`)
+    // }
+
+})
 

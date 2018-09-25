8a6ec5fb6bcbff0b22d7ec849a8fa0260961936a
Tiago Fernando Sarri <tiagosarri@hotmail.com>
2018-09-24 21:11:54 -0300

    * outra versão do js da manipulação de estilos

--- Semana 7/3 - Manipulação de estilos/2 - Menu/script/script.js
+++ Semana 7/3 - Manipulação de estilos/2 - Menu/script/script.js
@@ -3,36 +3,33 @@ const menuGeneral = document.querySelector(`.sidebar-menu`);
 const menuButton = document.querySelector(`.sidebar-menu__button`);
 
 menuButton.addEventListener(`click`, function(event){
-    event.preventDefault()
-
+    event.preventDefault();
 
     //aqui a sua variavel será do tipo objeto, porque tem os colchetes
-    var verifyPosition = [menuGeneral.style.left = `-34%`]
-    console.log(verifyPosition[0])
-    console.log(typeof verifyPosition) //vai retornar o tipo objeto ou seja [colchete] ou array de string
+    var verifyPosition = [menuGeneral.style.left = `-34%`];
+    console.log(verifyPosition[0]);
+    console.log(typeof verifyPosition); //vai retornar o tipo objeto ou seja [colchete] ou array de string
 
     if (verifyPosition[0] === `-34%`) {
-        menuGeneral.style.left = `0%`
+        menuGeneral.style.left = `0%`;
         console.log("1"); //vai printar este console
     } else {
-        menuGeneral.style.left = `-44%` 
+        menuGeneral.style.left = `-44%`;
         console.log("2");
     }
 
     //pego a mesma variavel, e faço de outro forma, sem os colchetes neste a variavel sera string
-    verifyPosition = (menuGeneral.style.left = `-34%`)
-    console.log(verifyPosition)
-    console.log(typeof verifyPosition) //vai retornar o tipo string porque esta sem os [colchete]
+    verifyPosition = (menuGeneral.style.left = `-34%`);
+    console.log(verifyPosition);
+    console.log(typeof verifyPosition); //vai retornar o tipo string porque esta sem os [colchete]
 
     if (verifyPosition === `-34%`) {
-        menuGeneral.style.left = `0%`
+        menuGeneral.style.left = `0%`;
         console.log("1"); //vai printar este console
     } else {
-        menuGeneral.style.left = `-44%` 
+        menuGeneral.style.left = `-44%`;
         console.log("2");
     }    
-
-
 
     // const verifyPosition = (menuGeneral.style.left = `-34%`)
 
